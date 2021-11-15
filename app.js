6dfffdf1cbb8ca1a72b6471d669abb0a00684af0
Toan Pham <toan.chpham@gmail.com>
2021-11-15 00:52:26 -0800

    removing reviews / removing campground

--- app.js
+++ app.js
@@ -138,6 +138,16 @@ app.post(
     })
 );
 
+app.delete(
+    "/campgrounds/:id/reviews/:reviewId",
+    catchAsync(async function (req, res) {
+        const { id, reviewId } = req.params;
+        await Campground.findByIdAndUpdate(id, { $pull: { review: reviewId } });
+        await Review.findByIdAndDelete(req.params.reviewId);
+        res.redirect(`/campgrounds/${id}`);
+    })
+);
+
 app.all("*", function (req, res, next) {
     next(new ExpressError("Page Not Found", 404));
 });
