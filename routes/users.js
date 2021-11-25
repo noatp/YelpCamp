abb54c6a69ea148d78b7d808df71e842c38653ea
Toan Pham <toan.chpham@gmail.com>
2021-11-24 20:35:13 -0800

    allow user to login

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/user");
 const catchAsync = require("../utils/catchAsync");
+const passport = require("passport");
 
 router.get("/register", function (req, res) {
     res.render("users/register");
@@ -23,4 +24,21 @@ router.post(
         }
     })
 );
+
+router.get("/login", function (req, res) {
+    res.render("users/login");
+});
+
+router.post(
+    "/login",
+    passport.authenticate("local", {
+        failureFlash: true,
+        failureRedirect: "/login",
+    }),
+    async function (req, res) {
+        req.flash("success", "Welcome back!");
+        res.redirect("/campgrounds");
+    }
+);
+
 module.exports = router;
